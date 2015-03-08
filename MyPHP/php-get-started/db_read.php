15c54ca108f36962d1c0bf023fefeb720cff28c8
Varun Malhotra <varunmalhotra149@gmail.com>
2015-03-08 04:03:39 -0700

    Adding db read code

---
+++ MyPHP/php-get-started/db_read.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP</title>
+</head>
+<body>
+<ul>
+	<?php
+		$db = mysqli_connect('localhost', 'root', '', 'php');
+		$sql = "SELECT * FROM users";
+		$result = mysqli_query($db, $sql);
+		foreach ($result as $row) {
+			printf('<li><span style="color: %s";>%s (%s)</span></li>', htmlspecialchars($row['color']), htmlspecialchars($row['name']), htmlspecialchars($row['gender']));
+		}
+	?>
+</ul>
+</body>
+</html>
