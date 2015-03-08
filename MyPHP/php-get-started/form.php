4c3b6c82320ee555978b1e44ffdf7879db0050d2
Varun Malhotra <varunmalhotra149@gmail.com>
2015-03-07 16:27:15 -0800

    Added Prefilling of the various form fields, also a bit of refactoring

--- MyPHP/php-get-started/form.php
+++ MyPHP/php-get-started/form.php
@@ -8,61 +8,81 @@
 		if(isset($_POST['submit']))
 		{
 			$ok = true;
+			$name = '';
+			$gender = '';
+			$colour = '';
+			$comments = '';
+			$tc = '';
+			$languages = array();
+
 			if (!isset($_POST['name']) || $_POST['name'] === '')
 				$ok = false;
+			else
+				$name = $_POST['name'];
 			if (!isset($_POST['password']) || $_POST['password'] === '')
 				$ok = false;
 			if (!isset($_POST['gender']) || $_POST['gender'] === '')
 				$ok = false;
+			else
+				$gender = $_POST['gender'];
 			if (!isset($_POST['colour']) || $_POST['colour'] === '')
 				$ok = false;
+			else
+				$colour = $_POST['colour'];
 			if (!isset($_POST['comments']) || $_POST['comments'] === '')
 				$ok = false;
+			else
+				$comments = $_POST['comments'];
 			if (!isset($_POST['tc']) || $_POST['tc'] === '')
 				$ok = false;
-			if (!isset($_POST['Languages']) || !is_array($_POST['Languages']) || count($_POST['Languages']) === 0) 
+			else
+				$tc = $_POST['tc'];
+			if (!isset($_POST['languages']) || !is_array($_POST['languages']) || count($_POST['languages']) === 0) 
 				$ok = false;
+			else
+				$languages = $_POST['languages'];
+
 			if($ok)
 			{
 				printf('UserName: %s <br>
 					   Password: %s <br>
 					   Gender: %s <br>
 					   Color: %s <br>
-					   Languages: %s <br>
+					   languages: %s <br>
 					   Comments: %s <br>
 					   T and C: %s <br>'
-					   ,htmlspecialchars($_POST['name'])
+					   ,htmlspecialchars($name)
 					   ,htmlspecialchars($_POST['password'])
-					   ,htmlspecialchars($_POST['gender'])
-					   ,htmlspecialchars($_POST['colour'])
-					   ,htmlspecialchars(implode(" ", $_POST['Languages']))
-					   ,htmlspecialchars($_POST['comments'])
-					   ,htmlspecialchars($_POST['tc'])
+					   ,htmlspecialchars($gender)
+					   ,htmlspecialchars($colour)
+					   ,htmlspecialchars(implode(" ", $languages))
+					   ,htmlspecialchars($comments)
+					   ,htmlspecialchars($tc)
 					   );
 			}
 		}
 	?>
 	<form method="post" action="">
-		User Name: <input type="text" name="name"><br>
+		User Name: <input type="text" name="name" value="<?php echo htmlspecialchars($name);?>"><br>
 		Password: <input type="password" name="password"><br>
 		Gender:
-		<input type="radio" name="gender" value="f">female
-		<input type="radio" name="gender" value="m">male<br>
+		<input type="radio" name="gender" value="f" <?php if($gender === "f") echo "checked";?>>female
+		<input type="radio" name="gender" value="m" <?php if($gender === "m") echo "checked";?>>male<br>
 		Favourite Color:
 		<select name="colour">
 			<option value="">Please Select a Colour</option>
-			<option value="#f00">red</option>
-			<option value="#0f0">green</option>
-			<option value="#00f">blue</option>
+			<option value="#f00" <?php if($colour === "#f00") echo "selected";?>>red</option>
+			<option value="#0f0" <?php if($colour === "#0f0") echo "selected";?>>green</option>
+			<option value="#00f" <?php if($colour === "#00f") echo "selected";?>>blue</option>
 		</select><br>
 		Languages Spoken:
-		<select name="Languages[]" multiple s ize="3">
-			<option value="en">English</option>
-			<option value="fr">French</option>
-			<option value="it">Italian</option>					
+		<select name="languages[]" multiple s ize="3">
+			<option value="en" <?php if(in_array("en", $languages)) echo "selected";?>>English</option>
+			<option value="fr" <?php if(in_array("fr", $languages)) echo "selected";?>>French</option>
+			<option value="it" <?php if(in_array("it", $languages)) echo "selected";?>>Italian</option>					
 		</select><br>
-		Comments: <textarea name="comments"></textarea><br>
-		<input type="checkbox" name="tc" value="ok">I accept T and C<br>
+		Comments: <textarea name="comments"><?php echo htmlspecialchars($comments);?></textarea><br>
+		<input type="checkbox" name="tc" value="ok" <?php if($tc === "ok") echo "checked";?>>I accept T and C<br>
 		<input type="submit" name="submit" value="Submit">			
 	</form>
 </body>
