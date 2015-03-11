edb605e4348358476a7cbe6de2871714581cab77
Varun Malhotra <varunmalhotra149@gmail.com>
2015-03-10 18:34:57 -0700

    Hello to C++

---
+++ C++/cast.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	int a;
+	double b = 2.2;
+	a = b;
+	std::cout<<"a = "<<a<<std::endl;
+	return 0;
+}
---
+++ C++/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+	std::string name;
+	std::cout<<"Type your name"<<std::endl;
+	std::cin >> name;
+	std::cout<<"Hello, "<<name<<std::endl;
+	return 0;
+}
