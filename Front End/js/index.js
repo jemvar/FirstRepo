aec13a40cd4c1acf730c4c5f747e88891e48381d
Varun Malhotra <varunmalhotra149@gmail.com>
2015-03-14 12:34:59 -0700

    Modifying the document using JQuery

--- Front End/js/index.js
+++ Front End/js/index.js
@@ -15,9 +15,46 @@ $(document).ready(function()
 		{
 			toggleButton.text("Hide");
 		};
-	})	
+	});
+	var fakeResults =
+	[ 
+	{
+		name:"jquery",
+		language: "JS",
+		score:4,
+		show: function() {},
+		owner:{ login:"jemvar"}
+	},
+	{
+		name:"Varun",
+		language: "Java",
+		score:4,
+		show: function() {},
+		owner:{ login:"repo"}
+	}
+	];
+	resultList.empty();
+	$.each(fakeResults, function (i, item) {
+		var newResult = $("<div class='result'>"+
+						"<div class='title'>"+item.name+"</div>"+
+						"<div>Language: "+item.language+"</div>"+
+						"<div>Owner: "+item.owner.login+"</div>"+
+						"</div>");
+		newResult.hover
+		(
+			function () 
+			{
+				$(this).css("background-color","lightgray");
+			},
+			function () 
+			{
+				$(this).css("background-color","transparent");
+			}
+		);
+		resultList.append(newResult);
+	});
 })
 
-var listItems = $("header nav li");
-listItems.css("font-weight","bold");
-listItems.filter(":first").css("font-size","18px");
+// var listItems = $("header nav li");
+// listItems.css("font-weight","bold");
+// listItems.filter(":first").css("font-size","18px");
