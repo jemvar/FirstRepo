5d74fe4e715bc41bf8c183710a84ca69c9093cb3
Varun Malhotra <varunmalhotra149@gmail.com>
2015-03-13 21:29:52 -0700

    Conditionals

--- Front End/js/index.js
+++ Front End/js/index.js
@@ -6,5 +6,33 @@ resultsDiv.innerHTML = "<p> This is from JS</p>";
 console.log(typeof(mssg));
 console.log(typeof(resultsDiv));
 var a;
+var num = 10;
 console.log(typeof(a));
 // bavv = "new str";
+
+// Conditionals
+
+if(a == undefined)
+{
+	console.log("a is undefined");
+}
+
+if(!a)
+{
+	console.log("!a is true");
+}
+
+if(!mssg)
+{
+	console.log("!mssg is true");
+}
+
+if(num == "10")
+{
+	console.log("type checking with ==")
+}
+
+if(num === "10")
+{
+	console.log("exact checking with ===")
+}
