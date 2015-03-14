c36a1a5aeb8fdef6a3f3c47d5a575630b9022be0
Varun Malhotra <varunmalhotra149@gmail.com>
2015-03-14 03:24:11 -0700

    Handling events using JQuery

--- Front End/js/index.js
+++ Front End/js/index.js
@@ -1,3 +1,19 @@
-"use strict";
-var resultList = $("#resultList");
-resultList.text("This is from JQuery");
+$(document).ready(function() 
+{
+	"use strict";
+
+	var resultList = $("#resultList");
+	resultList.text("This is from JQuery");
+	var toggleButton = $("#toggleButton");
+	toggleButton.on("click", function () {
+		resultList.toggle(1000);
+		if (toggleButton.text() == "Hide") 
+		{
+			toggleButton.text("Show");
+		} 
+		else
+		{
+			toggleButton.text("Hide");
+		};
+	})	
+};)
