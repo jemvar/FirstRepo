fb896907e53c3a477c895f88d89617eea1a2c01d
Varun Malhotra <varunmalhotra149@gmail.com>
2015-03-13 21:44:46 -0700

    Functions in JS

--- Front End/js/index.js
+++ Front End/js/index.js
@@ -1,38 +1,74 @@
 "use strict";
-var mssg = "hello JS";
-console.log(mssg);
-var resultsDiv = document.getElementById("results");
-resultsDiv.innerHTML = "<p> This is from JS</p>";
-console.log(typeof(mssg));
-console.log(typeof(resultsDiv));
-var a;
-var num = 10;
-console.log(typeof(a));
-// bavv = "new str";
+// var mssg = "hello JS";
+// console.log(mssg);
+// var resultsDiv = document.getElementById("results");
+// resultsDiv.innerHTML = "<p> This is from JS</p>";
+// console.log(typeof(mssg));
+// console.log(typeof(resultsDiv));
+// var a;
+// var num = 10;
+// console.log(typeof(a));
+// // bavv = "new str";
 
-// Conditionals
+// // Conditionals
 
-if(a == undefined)
+// if(a == undefined)
+// {
+// 	console.log("a is undefined");
+// }
+
+// if(!a)
+// {
+// 	console.log("!a is true");
+// }
+
+// if(!mssg)
+// {
+// 	console.log("!mssg is true");
+// }
+
+// if(num == "10")
+// {
+// 	console.log("type checking with ==")
+// }
+
+// if(num === "10")
+// {
+// 	console.log("exact checking with ===")
+// }
+
+// Function
+
+function show(mssg)
 {
-	console.log("a is undefined");
+	console.log("show1 "+mssg);
 }
+function show (mssg, more) {
+	console.log("show2 "+mssg+" "+more);
+}
+show("hello");
+show("hello","world");
 
-if(!a)
-{
-	console.log("!a is true");
+var showIt = function print (mssg) {
+	console.log("printing "+ mssg);
 }
+showIt("hello");
 
-if(!mssg)
-{
-	console.log("!mssg is true");
+function showItFinal (mssg, callback) {
+	console.log(mssg);
+	callback();
 }
+showItFinal("showItFinal called", function() {console.log("callback called");});
 
-if(num == "10")
-{
-	console.log("type checking with ==")
-}
 
-if(num === "10")
-{
-	console.log("exact checking with ===")
-}
+
+
+
+
+
+
+
+
+
+
+
